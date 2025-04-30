a5058d9e3b478e4c836a6c2e66c7720d0ccb9f3a
Bill Vandenberk <bill@vandenberk.me>
2025-04-30 18:55:45 -0400

    Revert "Update Helm release authentik to v2025.4.0"

--- modules/authentik-system/main.tf
+++ modules/authentik-system/main.tf
@@ -35,7 +35,7 @@ resource "helm_release" "authentik" {
   name = "authentik"
   repository = "https://charts.goauthentik.io"
   chart = "authentik"
-  version = "2025.4.0"
+  version = "2025.2.4"
   namespace = "authentik"
   create_namespace = true
   
