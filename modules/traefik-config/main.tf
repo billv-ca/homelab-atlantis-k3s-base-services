276cd66d6dfed6512e52a501aa4f7f95f650fa0c
Bill Vandenberk <bill@vandenberk.me>
2026-08-07 10:49:01 -0400

    working on traefik config

--- modules/traefik-config/main.tf
+++ modules/traefik-config/main.tf
@@ -1,4 +1,8 @@
 resource "kubernetes_manifest" "traefik_entrypoints" {
+  field_manager {
+    name           = "terraform"
+    force_conflicts = true
+  }
     manifest = {
         "apiVersion" = "helm.cattle.io/v1"
         "kind" = "HelmChartConfig"
@@ -36,9 +40,29 @@ ports:
         forwardedHeaders:
             trustedIPs:
                 - 10.0.0.0/8
+#        http:
+#            middlewares:
+#                cors:
+#                    headers:
+#                      accessControlAllowMethods:
+#                        - GET
+#                      accessControlAllowCredentials: true
+#                      accessControlAllowOriginListRegex:
+#                        - "https:\\/\\/.*\\.billv\\.ca"
+#                      accessControlMaxAge: 300
 logs:
     access:
         enabled: true
+
+#ingressRoute:
+#  dashboard:
+#    enabled: true
+#    matchRule: Host(`traefik.billv.ca`)
+#    services:
+#      - name: api@internal
+#        kind: TraefikService
+#    entryPoints: ["websecure"]
+#    middlewares: ["authentik@kube-system"]
 EOF
          }
     }
